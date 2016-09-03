9c2b421c1d2666415587e45ee99f5edeb3273da4
Adel Ardalan <adel@cs.wisc.edu>
2016-09-03 00:54:05 -0500

    Added Python3 sys package reload support.

--- py_valuenormalization/manual_value_normalization.py
+++ py_valuenormalization/manual_value_normalization.py
@@ -191,7 +191,6 @@ def normalize_values(vals):
 	for val in vals:
 		clusters[val]	= [val,]
 	norm_app	= ManualValueNormalizationAppProcess(clusters)
-	norm_app.daemon	= True
 	norm_app.start()
 	norm_app.join()
 	return norm_app.queue.get()
