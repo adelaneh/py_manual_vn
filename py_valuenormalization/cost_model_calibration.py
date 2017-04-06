b23175937ccee3878b0af72a76a4933e0d38741e
Adel Ardalan <adel@cs.wisc.edu>
2017-04-06 15:15:09 -0500

    Revert "PyQt5 on Linux."
    This reverts commit b7ccb2bb8be82926dd2c06a1064ce89c2dfad828.

--- py_valuenormalization/cost_model_calibration.py
+++ py_valuenormalization/cost_model_calibration.py
@@ -14,8 +14,7 @@ from copy import deepcopy
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-#from PyQt5.QtWebKit import *
-from PyQt5.QtWebKitWidgets import *
+from PyQt5.QtWebKit import *
 
 if (sys.version_info > (3, 0)):
 	from .value_normalization_misc import *
