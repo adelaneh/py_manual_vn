8e155eae0883afa0dec78d2ca7bbc11240432116
Adel Ardalan <adel@cs.wisc.edu>
2017-04-06 16:11:50 -0500

    PyQt5 on Mac.

--- py_valuenormalization/value_normalization_misc.py
+++ py_valuenormalization/value_normalization_misc.py
@@ -49,9 +49,9 @@ class Window(QMainWindow):
 		self.setCentralWidget(self.centralwidget)
 
 #		self._view.page().settings().setNetworkAccessManager(QNetworkAccessManager())
-		self._view.page().settings().setObjectCacheCapacities(0, 0, 0)
-		self._view.page().settings().setMaximumPagesInCache(0)
-		self._view.page().settings().setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
+#		self._view.page().settings().setObjectCacheCapacities(0, 0, 0)
+#		self._view.page().settings().setMaximumPagesInCache(0)
+#		self._view.page().settings().setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
 
 class ConsolePrinter(QObject):
 	def __init__(self, parent=None):
